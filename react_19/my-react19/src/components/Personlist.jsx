d5ff3c2ae1c0edb92f1fdfce89517926d3332afc
puttipong <puttipong.le0695@gmil.com>
2025-06-09 15:34:53 +0700

    insert image

--- react_19/my-react19/src/components/Personlist.jsx
+++ react_19/my-react19/src/components/Personlist.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import boy from "../assets/boy.svg";
+import girl from "../assets/girl.svg";
 
 function Personlist() {
     const [data, setData] = useState([
         { id: 1, name: "aun", gender: "male" },
         { id: 2, name: "num", gender: "female" },
         { id: 3, name: "fix", gender: "male" },
+        { id: 4, name: "ploy", gender: "female" },
     ]);
 
     const [show, setShow] = useState(true);
@@ -15,7 +18,9 @@ function Personlist() {
             <button onClick={() => setShow(!show)}>{show ? "hidden" : "show"}</button>
             <ul>
                 {show && data.map((item) => (
-                    <li key={item.id}>{item.name} | {item.gender}</li>
+                    <li key={item.id}>
+                        <img src={item.gender == "male" ? boy : girl} width={50} height={50} />
+                        {item.name}</li>
                 ))}
             </ul>
         </>
