ac14e2467fb1f008244e99fd3a0d12db8c8ec257
puttipong <puttipong.le0695@gmil.com>
2025-06-09 15:48:49 +0700

    Global Style

--- react_19/my-react19/src/App.jsx
+++ react_19/my-react19/src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./components/Header";
 import Personlist from "./components/Personlist";
 
 function App() {
-  
+
   return (
     <>
-    <Header/>
-    <Personlist/>
+      <Header />
+      <main>
+        <Personlist />
+      </main>
     </>
   )
 }
