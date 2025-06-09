a32cb24cb36942db90c2753439ce4b364f9efbb1
puttipong <puttipong.le0695@gmil.com>
2025-06-09 11:53:58 +0700

    useState learning

--- react_19/my-react19/src/App.jsx
+++ react_19/my-react19/src/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 function App() {
   const name = "Aun";
+  const [age, setAge] = useState(10)
+
   return (
-    <div>
+    <>
       <h1>Learning React 19 by {name}</h1>
-      <p>Let's GO !!!</p>
-    </div>
+      <p>Age: {age}</p>
+      <button onClick={() => setAge(age + 1)}>เพิ่มอายุ</button>
+      <button onClick={() => setAge(age - 1)}>ลดอายุ</button>
+      <button onClick={() => setAge(10)}>reset</button>
+    </>
   )
 }
 
