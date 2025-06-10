9dd9a66a941a8f1b45a0acf19ee50cba0c265997
puttipong <puttipong.le0695@gmil.com>
2025-06-10 13:27:59 +0700

    Component Style

--- react_19/my-react19/src/App.jsx
+++ react_19/my-react19/src/App.jsx
@@ -1,15 +1,16 @@
 import Header from "./components/Header";
 import Personlist from "./components/Personlist";
+import "./App.css";
 
 function App() {
 
   return (
-    <>
+    <div className="app">
       <Header />
       <main>
         <Personlist />
       </main>
-    </>
+    </div>
   )
 }
 
--- react_19/my-react19/src/components/Header.jsx
+++ react_19/my-react19/src/components/Header.jsx
@@ -1,7 +1,12 @@
+import "./Header.css";
+
 function Header(){
     return (
+        <nav>
         <h1>My App</h1>
-    )
-}
+        <button>Light/Dark</button>
+        </nav>
+    );
+};
 
 export default Header;
