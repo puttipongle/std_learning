8ad85b16b91eedcdca553965488803f5a29cf0d7
puttipong <puttipong.le0695@gmil.com>
2025-06-22 14:58:02 +0700

    Data type

--- javascript-learning/index.js
+++ javascript-learning/index.js
@@ -1,4 +1,17 @@
-const name = "John";
-let age = 30;
-console.log('Hello ' + name);
-console.log('You are ' + age + ' years old');
+// const name = "John";
+// let age = 30;
+
+const person = {
+    name: 'John',
+    age: 30,
+    sayHello: function () {
+        console.log('Hello ' + this.name, this.age + ' years old');
+    }
+}
+person.sayHello();
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
